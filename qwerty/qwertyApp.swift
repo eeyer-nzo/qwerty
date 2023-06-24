08fdd66b428cd34bdc01631aafd8631b60394041
eeyer-nzo <enzooh8888@gmail.com>
2023-06-24 12:01:33 +0800

    initial commit

---
+++ qwerty/qwertyApp.swift
@@ -0,0 +1,17 @@
+//
+//  qwertyApp.swift
+//  qwerty
+//
+//  Created by T Krobot on 24/6/23.
+//
+
+import SwiftUI
+
+@main
+struct qwertyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
